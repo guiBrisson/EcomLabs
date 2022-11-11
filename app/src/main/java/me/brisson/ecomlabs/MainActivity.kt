5e142a10debdef4d8f74cd66a6ef02d33426748a
guiBrisson <guibrisson@hotmail.com>
2022-11-11 18:22:48 -0300

    First commit

---
+++ app/src/main/java/me/brisson/ecomlabs/MainActivity.kt
@@ -0,0 +1,44 @@
+package me.brisson.ecomlabs
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import me.brisson.ecomlabs.ui.theme.EcomLabsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EcomLabsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", style = MaterialTheme.typography.titleLarge )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EcomLabsTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/me/brisson/ecomlabs/ui/theme/Type.kt
@@ -0,0 +1,62 @@
+package me.brisson.ecomlabs.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import me.brisson.ecomlabs.R
+
+val openSans = FontFamily(
+    Font(R.font.open_sans_light, weight = FontWeight.Light),
+    Font(R.font.open_sans_regular),
+    Font(R.font.open_sans_medium, weight = FontWeight.Medium),
+    Font(R.font.open_sans_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.open_sans_bold, weight = FontWeight.Bold),
+)
+
+val Typography = Typography(
+    labelLarge = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 0.1.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        letterSpacing = 0.4.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = openSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+)
