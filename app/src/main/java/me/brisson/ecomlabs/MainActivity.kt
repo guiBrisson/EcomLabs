9c0acea88d300f84f534f612717970c85b368f6f
guiBrisson <guibrisson@hotmail.com>
2022-11-12 13:45:21 -0300

    AppTopBar

--- app/src/main/java/me/brisson/ecomlabs/MainActivity.kt
+++ app/src/main/java/me/brisson/ecomlabs/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import dagger.hilt.android.AndroidEntryPoint
 import me.brisson.ecomlabs.presentation.home.HomeScreen
 import me.brisson.ecomlabs.ui.theme.EcomLabsTheme
 import me.brisson.ecomlabs.util.ConnectionUtils
 import me.brisson.ecomlabs.util.NoInternetConnectionComponent
 
 @ExperimentalMaterial3Api
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/me/brisson/ecomlabs/util/SharedComponents.kt
+++ app/src/main/java/me/brisson/ecomlabs/util/SharedComponents.kt
@@ -3,10 +3,9 @@ package me.brisson.ecomlabs.util
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
 import androidx.compose.material.icons.filled.WifiOff
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,6 +13,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.brisson.ecomlabs.R
+import me.brisson.ecomlabs.ui.theme.EcomLabsTheme
 
 @Composable
 @Preview
@@ -39,5 +39,44 @@ fun NoInternetConnectionComponent() {
             color = MaterialTheme.colorScheme.inverseOnSurface,
             style = MaterialTheme.typography.bodySmall
         )
+    }
+}
+
+@Composable
+@ExperimentalMaterial3Api
+fun AppTopBar(
+    modifier: Modifier = Modifier,
+    onBack: () -> Unit,
+    actions: @Composable RowScope.() -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier.fillMaxWidth(),
+        title = {
+            Text(
+                text = stringResource(id = R.string.search_appbar_title),
+                style = MaterialTheme.typography.titleLarge
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBack) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBackIos,
+                    contentDescription = stringResource(id = R.string.arrow_back_content_description)
+                )
+            }
+        },
+        windowInsets = WindowInsets(left = 20.dp, right = 20.dp),
+        actions = actions
+    )
+}
+
+
+
+@Preview
+@ExperimentalMaterial3Api
+@Composable
+fun PreviewTopAppBar() {
+    EcomLabsTheme {
+        AppTopBar(onBack = { })
     }
 }
